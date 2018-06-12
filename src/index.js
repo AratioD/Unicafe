6f20cdcd5987e06ff383f368cb0a3b973c411715
AratioD <antti.heroja@gmail.com>
2018-06-12 23:17:47 +0300

    1.7 still underdeployment

--- src/index.js
+++ src/index.js
@@ -5,34 +5,41 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      bad: 1,
-      neutral: 1,
-      good: 1,
+      bad: 0,
+      badValue: -1,
+      neutral: 0,
+      neutralValue: 0,
+      good: 0,
+      goodValue: 1,
+      buttonclick: 0,
       topic: "anna palautetta",
       otherTopic: "statistiikka",
     }
-
   }
 
   render() {
     return (
       <div>
         <h1>{this.state.topic}</h1>
-        <button onClick={() => this.setState({ good: this.state.good + 1 })}>
+        <button onClick={() => this.setState({ good: this.state.good + 1, buttonclick: this.state.buttonclick + 1 })}>
           hyvä
-          </button>
-        <button onClick={() => this.setState({ neutral: this.state.neutral + 1 })}>
+        </button>
+        <button onClick={() => this.setState({ neutral: this.state.neutral + 1, buttonclick: this.state.buttonclick + 1 })}>
           neutraali
-          </button>
-        <button onClick={() => this.setState({ bad: this.state.bad + 1 })}>
+        </button>
+        <button onClick={() => this.setState({ bad: this.state.bad + 1, buttonclick: this.state.buttonclick + 1 })}>
           huono
-          </button>
+        </button>
 
 
         <h1>{this.state.otherTopic}</h1>
         <div>hyvä {this.state.good}</div>
         <div>neutraali {this.state.neutral}</div>
         <div>huono {this.state.bad}</div>
+        <div>keskiarvo {(this.state.badValue*this.state.bad) +
+         (this.state.neutralValue*this.state.neutral) +
+         (this.state.goodValue*this.state.good)
+          / this.state.buttonclick}</div>
 
       </div>
     )
