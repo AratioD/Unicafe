700633485ce6a12839b22b7d663c9fe21f29769c
AratioD <antti.heroja@gmail.com>
2018-06-19 23:51:04 +0300

    1.8 under development

--- src/index.js
+++ src/index.js
@@ -17,16 +17,13 @@ class App extends React.Component {
       topic: "anna palautetta",
       otherTopic: "statistiikka",
     }
-    var value = 10;
-    value = value.toFixed(2);
-
   }
 
   render() {
     return (
       <div>
         <h1>{this.state.topic}</h1>
-        <button onClick={() => this.setState({ good: this.state.good + 1, buttonclick: this.state.buttonclick + 1, averageSum: this.state.averageSum + 1 })}>
+        <button onClick={() => this.setState({ good: this.state.good + 1, buttonclick: this.state.buttonclick + 1, averageSum: this.state.averageSum + 1, ButtonGood })}>
           hyvä
         </button>
         <button onClick={() => this.setState({ neutral: this.state.neutral + 1, buttonclick: this.state.buttonclick + 1 })}>
@@ -36,6 +33,12 @@ class App extends React.Component {
           huono
         </button>
 
+        {/* <button onClick={ButtonGood}>testi jee</button>
+
+        <button onClick={() => this.setState({ good: this.state.good + 1, buttonclick: this.state.buttonclick + 1, averageSum: this.state.averageSum + 1, ButtonGood })}>
+          testststst
+        </button> */}
+
 
         <h1>{this.state.otherTopic}</h1>
         <div>hyvä {this.state.good}</div>
@@ -43,11 +46,22 @@ class App extends React.Component {
         <div>huono {this.state.bad}</div>
         <div>keskiarvo {(this.state.averageSum / this.state.buttonclick).toFixed(1)} </div>
         <div>positiivisia {((this.state.good / this.state.buttonclick) * 100).toFixed(1)} %</div>
-
+        <ButtonGood topic={this.state.good} />
+        
       </div>
     )
   }
 }
+
+const ButtonGood = (props) => {
+  console.log("sdfsdxxx2")
+  return (
+    <div>
+      Hyvä {props.topic}
+    </div>
+  )
+}
+
 
 ReactDOM.render(
   <App />,
